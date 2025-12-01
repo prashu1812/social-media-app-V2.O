7337207e56b88b0c950fdee963405ab823fff822
prashu <str.prashu999@email.com>
2025-12-01 17:08:06 +0530

    added all maven files

---
+++ Desktop/devops/Maven/Ranga/Ranga_002/src/main/java/com/ranga/devops/App.java
@@ -0,0 +1,13 @@
+package com.ranga.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
